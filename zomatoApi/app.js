f80dfb44c51874d660c4de536102f2a001fd43da
Naveena Ponna <Naveena@welkin-digital.com>
2024-04-11 09:45:15 +0530

    zomatoapi basic  structure

---
+++ zomatoApi/app.js
@@ -0,0 +1,29 @@
+let express = require ('express');
+let mongo = require ('mongo');
+let dotenv = require ('dotenv');
+dotenv.config()
+let app = express();
+let MongoClient = mongo.MongoClient;
+let port = process.env.PORT;
+let cors = require('cors');
+let bodyParser = require ('body-parse');
+let db=db;
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cors());
+
+// get heart beat
+app.get('/',(req,res) => {
+    res.status(200).send('Health ok')
+})
+
+
+MongoClient.connect(mongoUrl,{useNewUrlParser:true},(err,client) => {
+    if(err) console.log(`Error While connecting to mongo`);
+    db =client.db('internfeb');
+    app.listen(port,() => {
+        console.log(`Running on port ${port}`)
+    })
+})
